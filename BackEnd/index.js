81322d33f9fb6a7c349654a8ef287778afa373ed
RakulAgn <rakul0agn@gmail.com>
2021-10-01 20:52:40 +0530

    Initial Commit

---
+++ BackEnd/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UserName 
+// User Mail id 
+// Event Name 
+// Date
+
+
